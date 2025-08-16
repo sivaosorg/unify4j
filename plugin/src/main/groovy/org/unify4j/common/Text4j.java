24460a1e86a817c18c5bbaf02ac1410aff92db42
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-08-16 23:48:21 +0700

    :art: style: update codebase #2

--- plugin/src/main/groovy/org/unify4j/common/Text4j.java
+++ plugin/src/main/groovy/org/unify4j/common/Text4j.java
@@ -496,6 +496,66 @@ public class Text4j {
     }
 
     /**
+     * Conditionally appends the given string enclosed in square brackets based on the provided condition.
+     *
+     * @param condition If true, the string will be appended; otherwise, no action is taken.
+     * @param str       The string to enclose in square brackets if the condition is true.
+     * @return The current instance of Text4j.
+     */
+    public Text4j bracketIf(boolean condition, String str) {
+        if (condition && String4j.isNotEmpty(str)) {
+            return this.bracket(str);
+        }
+        return this;
+    }
+
+    /**
+     * Conditionally appends the string representation of the given object enclosed in square brackets based on the provided condition.
+     * If the object is a primitive, its string value is used. Otherwise, its JSON representation is used.
+     *
+     * @param condition If true, the object's string representation will be appended; otherwise, no action is taken.
+     * @param value     The object to enclose in square brackets if the condition is true.
+     * @return The current instance of Text4j.
+     */
+    public Text4j bracketIf(boolean condition, Object value) {
+        if (condition && value != null) {
+            return this.bracket(Class4j.isPrimitive(value.getClass()) ? value.toString() : Json4j.toJson(value));
+        }
+        return this;
+    }
+
+    /**
+     * Conditionally appends the given string enclosed in square brackets repeated a specified number of times based on the provided condition.
+     *
+     * @param condition If true, the string will be appended; otherwise, no action is taken.
+     * @param value     The text to enclose in square brackets if the condition is true.
+     * @param repeat    The number of times to repeat the text.
+     * @return The current instance of Text4j.
+     */
+    public Text4j bracketIf(boolean condition, Object value, int repeat) {
+        if (condition && value != null) {
+            String str = Class4j.isPrimitive(value.getClass()) ? value.toString() : Json4j.toJson(value);
+            return this.bracket(String4j.repeat(str, repeat));
+        }
+        return this;
+    }
+
+    /**
+     * Conditionally appends the given string enclosed in square brackets repeated a specified number of times based on the provided condition.
+     *
+     * @param condition If true, the string will be appended; otherwise, no action is taken.
+     * @param str       The text to enclose in square brackets if the condition is true.
+     * @param repeat    The number of times to repeat the text.
+     * @return The current instance of Text4j.
+     */
+    public Text4j bracketIf(boolean condition, String str, int repeat) {
+        if (condition && String4j.isNotEmpty(str)) {
+            return this.bracket(String4j.repeat(str, repeat));
+        }
+        return this;
+    }
+
+    /**
      * Appends the given string enclosed in curly brackets.
      *
      * @param str The string to enclose in curly brackets.
